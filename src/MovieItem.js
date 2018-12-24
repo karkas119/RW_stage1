fc90e877956a69c68ac8af79e8134dc939fee9b1
Rustem <35926818+karkas119@users.noreply.github.com>
2018-12-24 14:53:46 +0200

    Update MovieItem.js

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -3,12 +3,6 @@ import "./index.css";
 import MoviesData from './moviesData'
 import MovieShow from './MovieShow'
 
-
-
-const link = MoviesData.map(function (MoviesData) {
-    return MoviesData.poster_path
-});
-
 class MovieList extends Component {
     constructor() {
         super();
